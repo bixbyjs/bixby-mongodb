1ef37d08c237ea25eed864560fbc9ad93e7adf22
Jared Hanson <jaredhanson@gmail.com>
2019-05-17 13:32:56 -0700

    Add tests.

--- test/service.test.js
+++ test/service.test.js
@@ -2,6 +2,7 @@ var $require = require('proxyquire');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var factory = require('../app/service');
+var mongodb = require('mongodb');
 
 
 describe('service', function() {
@@ -15,5 +16,56 @@ describe('service', function() {
     expect(factory['@implements']).to.deep.equal([ 'http://i.bixbyjs.org/mongodb', 'http://i.bixbyjs.org/IService' ]);
     expect(factory['@name']).to.equal('mongodb');
   });
+  
+  describe('API', function() {
+    var _keyring = { get: function(){} };
+    var _client = new mongodb.MongoClient();
+    var MongoClientStub = sinon.stub().returns(_client);
+    var api = $require('../app/service',
+      { 'mongodb': { MongoClient: MongoClientStub } }
+    )(_keyring);
+    
+    
+    describe('.createConnection', function() {
+      beforeEach(function() {
+        sinon.stub(_keyring, 'get').yieldsAsync(null, { username: 'root', password: 'keyboard cat' });
+        
+        sinon.stub(_client, 'connect').callsFake(function() {
+          var self = this;
+          process.nextTick(function() {
+            self.emit('open');
+          });
+        });
+      });
+      
+      afterEach(function() {
+        MongoClientStub.resetHistory();
+      });
+      
+      it('should construct client and connect', function(done) {
+        var client = api.createConnection({ cname: 'mongodb.example.com', port: 27017 });
+        
+        expect(MongoClientStub).to.have.been.calledOnceWithExactly('mongodb://mongodb.example.com:27017/insignature-tokens-development').and.calledWithNew;
+        expect(client).to.be.an.instanceof(mongodb.MongoClient);
+        
+        client.once('open', function() {
+          expect(client.s.options).to.deep.equal({ auth: { user: 'root', password: 'keyboard cat' } });
+          done();
+        });
+      }); // should construct client and connect
+      
+      it('should construct client, add listener and connect', function(done) {
+        var client = api.createConnection({ cname: 'mongodb.example.com', port: 27017 }, function() {
+          expect(client.s.options).to.deep.equal({ auth: { user: 'root', password: 'keyboard cat' } });
+          done();
+        });
+        
+        expect(MongoClientStub).to.have.been.calledOnceWithExactly('mongodb://mongodb.example.com:27017/insignature-tokens-development').and.calledWithNew;
+        expect(client).to.be.an.instanceof(mongodb.MongoClient);
+      }); // should construct client, add listener and connect
+      
+    }); // .createConnection
+    
+  }); // API
   
 });
