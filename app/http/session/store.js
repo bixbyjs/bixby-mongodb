8af7ab9ce16ddbf4bfae662a332998a9b317b9f6
Jared Hanson <jaredhanson@gmail.com>
2019-05-17 11:55:15 -0700

    Clean up session store.

--- app/http/session/store.js
+++ app/http/session/store.js
@@ -6,50 +6,9 @@ exports = module.exports = function(mongodb) {
   var api = {};
   
   api.createConnection = function(options, connectListener) {
-    console.log('CREATE MONGO SESSIONS CLIENT!');
-    console.log(options);
-    
-    
-    
-    
-    var client = mongodb.createConnection(options, function() {
-      console.log('STORE CONNECTED CALLBACK!');
-    });
-    
-    /*
-    var promise = new Promise(function(resolve, reject) {
-      client.on('open', function() {
-        console.log('MONGO OPEN FOR SESSION?');
-        //resolve(this);
-      });
-      
-      
-      client.on('__foo', function() {
-        console.log('MONGO OPEN FOR __FOO?');
-        resolve(this);
-      });
-    });
-    */
-    
-    
-    
+    var client = mongodb.createConnection(options);
     var store = new MongoStore({ client: client });
-    //var store = new MongoStore({ clientPromise: promise });
-    
-    store.on('connect', function() {
-      console.log('STORE CONNECT');
-    });
-    
-    store.on('connected', function() {
-      console.log('STORE CONNECTED!');
-    });
-    store.once('connected', connectListener);
-    
-    store.on('create', function() {
-      console.log('STORE CREATED!');
-    });
-    
-    //store.once('connect', connectListener);
+    if (connectListener) { store.once('connected', connectListener); }
     return store;
   }
   
